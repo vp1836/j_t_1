03c980d023d89a578360ee67bb8c9e419147145f
vp183 <vp1836@gmail.com>
2021-09-23 12:15:20 +0300

    [main]: add test Selenium search

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -12,12 +14,28 @@ public class FirstTest {
         WebDriver driver;
 //        System.setProperty("webdriver.chrome.driver", "C:\\Users\\vp183\\Desktop\\Code\\_JAVA\\chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+
+//        driver = new ChromeDriver();
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         driver = new ChromeDriver(options);
+
         driver.get("https://www.google.com/");
+
 //        driver.findElement(By.xpath("//button[@id=\"L2AGLb\"]")).click();
-        System.out.println(driver.getTitle());
-        Assertions.assertEquals("Google", driver.getTitle());
+
+        WebElement searchField = driver.findElement(By.xpath("//input[@name='q']"));
+        searchField.sendKeys("selenium");
+        searchField.sendKeys(Keys.ENTER);
+
+        String searchResult = driver.findElement(By.xpath("//h3[@class=\"LC20lb DKV0Md\"]")).getText();
+        Assertions.assertEquals("Selenium", searchResult);
+        System.out.println(searchResult);
+
+//        System.out.println(driver.getTitle());
+//        Assertions.assertEquals("selenium - Google meklēšana", driver.getTitle());
+
+        driver.quit();
     }
 }
