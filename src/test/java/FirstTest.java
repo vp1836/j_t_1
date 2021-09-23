1235bc85fa1a0922734f753106ac0e661e3a4eff
vp183 <vp1836@gmail.com>
2021-09-23 11:56:06 +0300

    [main]: remove click Accept

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -10,12 +10,13 @@ public class FirstTest {
     @Test
     public void OpenBrowser() {
         WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\vp183\\Desktop\\Code\\_JAVA\\chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         driver = new ChromeDriver(options);
         driver.get("https://www.google.com/");
-        driver.findElement(By.xpath("//button[@id=\"L2AGLb\"]")).click();
+//        driver.findElement(By.xpath("//button[@id=\"L2AGLb\"]")).click();
         System.out.println(driver.getTitle());
         Assertions.assertEquals("Google", driver.getTitle());
     }
